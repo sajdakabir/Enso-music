cb93b816d58d3b82c085efabfd83c8b7c444f506
sajdkabir <sajda.kbir@gmail.com>
2024-12-26 22:57:09 +0530

    feat: added auth url

--- src/loaders/environment.loader.ts
+++ src/loaders/environment.loader.ts
@@ -4,5 +4,7 @@ export const environment = () => {
   return {
     DATABASE_URL: process.env.DATABASE_URL,
     PORT: process.env.PORT || 9999,
+    SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+    REDIRECT_URI: process.env.REDIRECT_URI,
   };
 };
