e72c2ec53ac65ec716664b1733c76eb977359b81
sajdkabir <sajda.kbir@gmail.com>
2024-12-27 01:08:30 +0530

    feat: user schema update

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,7 +3,10 @@ import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
   
 export const users = pgTable("users",{
     id: uuid("id").primaryKey().defaultRandom(),
-    name: varchar("name"),
+    displayName: varchar("displayName"),
     email: varchar("email").unique(),
+    profileUrl: varchar("profileUrl"),
+    image: varchar("image"),
     createdAt: timestamp("created_at").defaultNow(),
+    updatedAt: timestamp("updated_at"),
 })
