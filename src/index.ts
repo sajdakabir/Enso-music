64a15584deb472c7d96cd3a74a3db68a2508d56f
sajdkabir <sajda.kbir@gmail.com>
2024-12-26 22:24:11 +0530

    chore: remove unused code

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,8 @@
 import { Elysia } from "elysia";
 import { environment } from "./loaders/environment.loader";
-import { db } from "./db/drizzle";
-import { users } from "./db/schema";
+
 
 const { PORT } = environment();
-
-await db.insert(users).values({
-  name: 'John Doe',
-  email: 'john.doe@example.com'
-});
-
-const user = await db.query.users.findFirst();
-console.log(user)
 
 const app = new Elysia().get("/", () => {
   return {
