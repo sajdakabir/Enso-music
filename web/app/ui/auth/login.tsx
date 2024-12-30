01c852b22b0eb87e5f025650c4c1ad15d8026991
sajdkabir <sajda.kbir@gmail.com>
2024-12-30 21:09:29 +0530

    ui: login

--- web/app/ui/auth/login.tsx
+++ web/app/ui/auth/login.tsx
@@ -2,12 +2,19 @@
 
 const InitialUi = () => {
     return (
-        <section>
-            <div>
-               <img src="/new_logo.png" alt="logo" />
+        <section className="flex flex-col items-center justify-center h-screen">
+            <div className="w-80 md:w-[500px] md:px-8 mx-auto">
+            <div className="flex flex-row items-center gap-x-4 justify-center">
+               <img height={35} width={35} src="/new_logo.png" alt="logo" />
+               <h2 className="text-white text-base font-bold">Welcome to Enso</h2>
             </div>
-            <div>
+            <div className="flex flex-col gap-3 mt-6 md:px-8 2xl:px-20">
 
+               <button className="bg-white text-gray-700 px-4 py-2 rounded-md text-sm font-semibold">
+                
+                <span>Continue with Spotify</span>
+               </button>
+            </div>
             </div>
         </section>
     )
